d4bd1c9057e85ba5e0ebdf6c464520584550d411
Matt Wistrand <matt@troglodytidae.com>
2017-12-20 15:30:14 -0600

    Fix: allow string mid in i18n template
    Account for the possibility that a plugin converts mids to strings (as
    opposed to the default numerical ids) when injecting the i18n template
    into the entry point.

--- tests/unit/i18n-plugin/dependencies/InjectedModuleDependency.ts
+++ tests/unit/i18n-plugin/dependencies/InjectedModuleDependency.ts
@@ -30,6 +30,20 @@ describe('InjectedModuleDependency', () => {
 		assert.sameDeepMembers(source.insertions, [[0, 'var answer = __webpack_require__(42);\n']]);
 	});
 
+	it('should account for string mids', () => {
+		const source = new ReplaceSource('');
+		const dep = new InjectedModuleDependency('/resource');
+		const template = new InjectedModuleDependency.Template();
+		const id = '/path/to/module.js';
+		const module = { id } as any;
+
+		dep.variable = 'answer';
+		dep.module = module;
+		template.apply(dep, source);
+
+		assert.sameDeepMembers(source.insertions, [[0, `var answer = __webpack_require__(${JSON.stringify(id)});\n`]]);
+	});
+
 	it('inject a missing module error without a module', () => {
 		const source = new ReplaceSource('');
 		const dep = new InjectedModuleDependency('/resource');

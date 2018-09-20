8d8b24f095c249869c1282eccc8a6a1b8261b50a
Anthony Gubler <agubler@sitepen.com>
2018-09-20 22:32:02 +0100

    use fs-extra

--- src/webpack-bundle-analyzer/viewer.ts
+++ src/webpack-bundle-analyzer/viewer.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 import * as glob from 'glob';
 import * as mkdir from 'mkdirp';
 import * as analyzer from './analyzer';

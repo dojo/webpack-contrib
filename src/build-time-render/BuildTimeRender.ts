36dbdb3b2670c1e417009c7b1a5d00ccee2e57db
Anthony Gubler <agubler@sitepen.com>
2018-11-07 09:29:34 +0000

    Support history api and statically generated html per route

--- src/build-time-render/BuildTimeRender.ts
+++ src/build-time-render/BuildTimeRender.ts
@@ -1,7 +1,7 @@
 import { Compiler } from 'webpack';
-import { readFileSync, writeFileSync, existsSync } from 'fs';
+import { readFileSync, outputFileSync, existsSync } from 'fs-extra';
 import * as path from 'path';
-
+const filterCss = require('filter-css');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
@@ -17,11 +17,18 @@ export interface BuildTimePath {
 	match: string[];
 }
 
+export interface RenderResult {
+	path: string;
+	html: string;
+	styles: string;
+}
+
 export interface BuildTimeRenderArguments {
 	root: string;
 	entries: string[];
 	useManifest?: boolean;
 	paths?: (BuildTimePath | string)[];
+	useHistory?: boolean;
 }
 
 class BuildTimeRender {
@@ -32,50 +39,162 @@ class BuildTimeRender {
 	private _useManifest: boolean;
 	private _hasPaths: boolean;
 	private _btrRoot: string;
+	private _useHistory = false;
+	private _manifest: any;
+	private _output: string;
+	private _originalEntries: string[];
 
 	constructor(args: BuildTimeRenderArguments) {
-		const { paths = [], root = '', useManifest = false, entries } = args;
-		this._paths = ['', ...paths];
+		const { paths = [], root = '', useManifest = false, entries, useHistory } = args;
+		let initialPath = paths[0];
+		initialPath = typeof initialPath === 'object' ? initialPath.path : initialPath;
+		this._useHistory = useHistory !== undefined ? useHistory : /^\#.*/.test(initialPath);
+		this._paths = paths;
 		this._hasPaths = paths.length > 0;
 		this._root = root;
-		this._entries = entries;
+		this._entries = [...entries];
 		this._useManifest = useManifest;
-		if (root === '') {
+		this._originalEntries = [...entries];
+		if (root === '' || paths.length === 0) {
 			this._disabled = true;
 		}
 	}
 
-	private _render(compiler: Compiler, location: string, htmlContent: string, root: string, output: string) {
-		const window: Window = new JSDOM(htmlContent, { runScripts: 'outside-only', pretendToBeVisual: true }).window;
+	private _generateRouteInjectionScript(html: string[]): string {
+		let replacement = '';
+		if (this._hasPaths) {
+			replacement = `<script>
+(function () {
+	var paths = ${JSON.stringify(this._paths)};
+	var html = ${JSON.stringify(html)};
+	var element = document.getElementById('${this._root}');
+	var target;
+	paths.some(function (path, i) {
+		var match = (typeof path === 'string' && path === window.location.hash) || path && (typeof path === 'object' && path.match && new RegExp(path.match.join('|')).test(window.location.hash));
+		if (match) {
+			target = html[i];
+		}
+		return match;
+	});
+	if (target && element) {
+		var frag = document.createRange().createContextualFragment(target);
+		element.parentNode.replaceChild(frag, element);
+	}
+}())
+</script>`;
+		}
+		return replacement;
+	}
+
+	private _filterCss(classes: string[], cssFiles: string[]) {
+		return cssFiles.reduce((result, entry: string) => {
+			const filteredCss = filterCss(path.join(this._output, entry), (context: string, value: string) => {
+				if (context === 'selector') {
+					value = value.replace(/(:| ).*/, '');
+					value = value.split('.')[0];
+					const firstChar = value.substr(0, 1);
+					if (classes.indexOf(value) !== -1 || ['.', '#'].indexOf(firstChar) === -1) {
+						return false;
+					}
+					return true;
+				}
+			})
+				.replace(/\/\*.*\*\//g, '')
+				.replace(/^(\s*)(\r\n?|\n)/gm, '');
+			result = `${result}\n${filteredCss}`;
+			return result;
+		}, '');
+	}
+
+	private _writeIndexHtml(
+		indexContent: string,
+		html: string,
+		styles: string,
+		cssFiles: string[],
+		route: string = '',
+		other: string = ''
+	) {
+		let updatedIndexContent = indexContent;
+		const prefix = this._getPrefix(route);
+		const css = cssFiles.reduce((css, entry: string | undefined) => {
+			updatedIndexContent = updatedIndexContent.replace(
+				`<link href="${entry}" rel="stylesheet">`,
+				`<style>${styles}</style>`
+			);
+			css = `${css}<link rel="stylesheet" href="${prefix}${entry}" media="none" onload="if(media!='all')media='all'" />`;
+			return css;
+		}, '');
+
+		updatedIndexContent = updatedIndexContent.replace(
+			this._createScripts(),
+			`${other}${css}${this._createScripts(route)}`
+		);
+		if (!this._hasPaths || this._useHistory) {
+			updatedIndexContent = updatedIndexContent.replace(this._btrRoot, html);
+		}
+		outputFileSync(path.join(this._output, ...route.split('/'), 'index.html'), updatedIndexContent);
+	}
+
+	private _getPrefix(route = '') {
+		return route
+			? `${route
+					.split('/')
+					.map(() => '..')
+					.join('/')}/`
+			: '';
+	}
+
+	private _createScripts(route = '') {
+		const prefix = this._getPrefix(route);
+		return this._originalEntries.reduce((script, entry) => {
+			entry = this._useManifest ? this._manifest[`${entry}.js`] : `${entry}.js`;
+			script = `${script}<script type="text/javascript" src="${prefix}${entry}"></script>`;
+			return script;
+		}, '');
+	}
+
+	private _render(
+		compiler: Compiler,
+		location: string,
+		htmlContent: string,
+		root: string,
+		cssFiles: string[]
+	): Promise<RenderResult> {
+		const window: Window = new JSDOM(htmlContent, {
+			url: `http://localhost/${location}`,
+			runScripts: 'outside-only',
+			pretendToBeVisual: true
+		}).window;
 		const document: Document = window.document;
 		const parent = document.getElementById(root)!;
 		if (!this._btrRoot) {
 			this._btrRoot = parent.outerHTML;
 		}
-		let manifest: any = {};
-		if (this._useManifest) {
-			manifest = JSON.parse(readFileSync(path.join(output, 'manifest.json'), 'utf-8'));
-		}
 		window.eval(`
-window.location.hash = '${location}';
 window.DojoHasEnvironment = { staticFeatures: { 'build-time-render': true } };`);
 
 		this._entries.forEach((entry) => {
-			entry = this._useManifest ? manifest[`${entry}.js`] : `${entry}.js`;
-			const entryContent = readFileSync(path.join(output, entry), 'utf-8');
+			const entryContent = readFileSync(path.join(this._output, `${entry}.js`), 'utf-8');
 			window.eval(entryContent);
 		});
 
-		const treeWalker = document.createTreeWalker(document.body, window.NodeFilter.SHOW_ELEMENT);
-		let classes: string[] = [];
+		const promise = new Promise<RenderResult>((resolve) => {
+			setTimeout(() => {
+				const treeWalker = document.createTreeWalker(document.body, window.NodeFilter.SHOW_ELEMENT);
+				let classes: string[] = [];
 
-		while (treeWalker.nextNode()) {
-			const node = treeWalker.currentNode as HTMLElement;
-			node.classList.length && classes.push.apply(classes, node.classList);
-		}
+				while (treeWalker.nextNode()) {
+					const node = treeWalker.currentNode as HTMLElement;
+					node.classList.length && classes.push.apply(classes, node.classList);
+				}
 
-		classes = classes.map((className) => `.${className}`);
-		return { html: parent.outerHTML, classes };
+				classes = classes.map((className) => `.${className}`);
+				const styles = this._filterCss(classes, cssFiles);
+				const html = parent.outerHTML.replace(/src="(.*?)"/g, `src="${this._getPrefix(location)}$1"`);
+				resolve({ html, styles, path: location });
+			}, 500);
+		});
+		return promise;
 	}
 
 	public apply(compiler: Compiler) {
@@ -83,99 +202,66 @@ window.DojoHasEnvironment = { staticFeatures: { 'build-time-render': true } };`)
 			return;
 		}
 		compiler.plugin('done', () => {
-			const output = compiler.options.output && compiler.options.output.path;
-			if (!output) {
+			if (compiler.options.output && compiler.options.output.path) {
+				this._output = compiler.options.output && compiler.options.output.path;
+				if (this._useManifest) {
+					this._manifest = JSON.parse(readFileSync(path.join(this._output, 'manifest.json'), 'utf-8'));
+					const extraEntries = Object.keys(this._manifest)
+						.filter(
+							(key) => this._entries.indexOf(`${key.replace('.js', '')}`) === -1 && /.*\.js$/.test(key)
+						)
+						.map((key) => this._manifest[key].replace('.js', ''));
+					this._entries = [this._manifest['runtime'], ...extraEntries, this._manifest['main']];
+				}
+			} else {
 				return;
 			}
-			let htmlContent = readFileSync(path.join(output, 'index.html'), 'utf-8');
-			let manifest: any = {};
-			if (this._useManifest) {
-				manifest = JSON.parse(readFileSync(path.join(output, 'manifest.json'), 'utf-8'));
-			}
-			const filterCss = require('filter-css');
+			let htmlContent = readFileSync(path.join(this._output, 'index.html'), 'utf-8');
+			const cssFiles = this._originalEntries.reduce(
+				(cssFiles, entry) => {
+					if (this._useManifest && this._manifest[`${entry}.css`]) {
+						cssFiles.push(this._manifest[`${entry}.css`]);
+					} else {
+						entry = `${entry}.css`;
+						const cssExists = existsSync(path.join(this._output, entry));
+						if (cssExists) {
+							cssFiles.push(entry);
+						}
+					}
+					return cssFiles;
+				},
+				[] as string[]
+			);
 
-			let html: string[] = [];
-			let classes: string[] = [];
-			const cssFiles: string[] = [];
-
-			this._paths.forEach((path) => {
+			const renderPromises = this._paths.map((path) => {
 				path = typeof path === 'object' ? path.path : path;
-				const result = this._render(compiler, path, htmlContent, this._root, output);
-				classes = [...classes, ...result.classes];
-				html = [...html, result.html];
+				return this._render(compiler, path, htmlContent, this._root, cssFiles);
 			});
 
-			const result = this._entries.reduce((result, entry: string | undefined) => {
-				if (this._useManifest) {
-					entry = manifest[`${entry}.css`];
+			Promise.all(renderPromises).then((results) => {
+				if (this._useHistory) {
+					results.map((result) => {
+						this._writeIndexHtml(htmlContent, result.html, result.styles, cssFiles, result.path);
+					});
 				} else {
-					entry = `${entry}.css`;
-					const cssExists = existsSync(path.join(output, entry));
-					if (!cssExists) {
-						entry = undefined;
-					}
+					const combinedResults = results.reduce(
+						(combined, result) => {
+							combined.styles = `${combined.styles}\n${result.styles}`;
+							combined.html.push(result.html);
+							return combined;
+						},
+						{ styles: '', html: [] } as { styles: string; html: string[] }
+					);
+					this._writeIndexHtml(
+						htmlContent,
+						combinedResults.html[0],
+						combinedResults.styles,
+						cssFiles,
+						'',
+						this._generateRouteInjectionScript(combinedResults.html)
+					);
 				}
-				if (entry) {
-					cssFiles.push(entry);
-					const filteredCss = filterCss(path.join(output, entry), (context: string, value: string) => {
-						if (context === 'selector') {
-							value = value.replace(/(:| ).*/, '');
-							const firstChar = value.substr(0, 1);
-							if (classes.indexOf(value) !== -1 || ['.', '#'].indexOf(firstChar) === -1) {
-								return false;
-							}
-							return true;
-						}
-					})
-						.replace(/\/\*.*\*\//g, '')
-						.replace(/^(\s*)(\r\n?|\n)/gm, '');
-					result = `${result}\n${filteredCss}`;
-				}
-				return result;
-			}, '');
-
-			let replacement = '';
-			if (this._hasPaths) {
-				replacement = `<script>
-	(function () {
-		var paths = ${JSON.stringify(this._paths)};
-		var html = ${JSON.stringify(html)};
-		var element = document.getElementById('${this._root}');
-		var target;
-		paths.some(function (path, i) {
-			var match = (typeof path === 'string' && path === window.location.hash) || path && (typeof path === 'object' && path.match && new RegExp(path.match.join('|')).test(window.location.hash));
-			if (match) {
-				target = html[i];
-			}
-			return match;
-		});
-		if (target && element) {
-			var frag = document.createRange().createContextualFragment(target);
-			element.parentNode.replaceChild(frag, element);
-		}
-	}())
-</script>`;
-			}
-
-			const script = this._entries.reduce((script, entry) => {
-				entry = this._useManifest ? manifest[`${entry}.js`] : `${entry}.js`;
-				script = `${script}<script type="text/javascript" src="${entry}"></script>`;
-				return script;
-			}, '');
-			const css = cssFiles.reduce((css, entry: string | undefined) => {
-				htmlContent = htmlContent.replace(
-					`<link href="${entry}" rel="stylesheet">`,
-					`<style>${result}</style>`
-				);
-				css = `${css}<link rel="stylesheet" href="${entry}" media="none" onload="if(media!='all')media='all'" />`;
-				return css;
-			}, '');
-
-			htmlContent = htmlContent.replace(script, `${replacement}${css}${script}`);
-			if (!this._hasPaths) {
-				htmlContent = htmlContent.replace(this._btrRoot, html[0]);
-			}
-			writeFileSync(path.join(output, 'index.html'), htmlContent);
+			});
 		});
 	}
 }

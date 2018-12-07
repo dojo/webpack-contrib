2acb7da0d42f9804c5778b45c6876f3f17f9159e
Anthony Gubler <agubler@sitepen.com>
2018-12-07 14:00:56 +0000

    Add sync and async bootstrap files

--- src/bootstrap-plugin/async.ts
+++ src/bootstrap-plugin/async.ts
@@ -1,18 +1,22 @@
 import has from '@dojo/framework/has/has';
 import '@dojo/framework/shim/Promise';
+import './sync';
 
-var modules = [];
+let modules = [];
 
+// @ts-ignore
 if (has(__dojoframeworkshimIntersectionObserver) && !has('dom-intersection-observer')) {
 	modules.push(
 		import(/* webpackChunkName: "platform/IntersectionObserver" */ '@dojo/framework/shim/IntersectionObserver')
 	);
 }
 
+// @ts-ignore
 if (has(__dojoframeworkshimResizeObserver) && !has('dom-webanimation')) {
 	modules.push(import(/* webpackChunkName: "platform/WebAnimations" */ '@dojo/framework/shim/WebAnimations'));
 }
 
+// @ts-ignore
 if (has(__dojoframeworkshimWebAnimations) && !has('dom-resize-observer')) {
 	modules.push(import(/* webpackChunkName: "platform/ResizeObserver" */ '@dojo/framework/shim/ResizeObserver'));
 }
@@ -22,5 +26,6 @@ if (!has('dom-pointer-events')) {
 }
 
 Promise.all(modules).then(function() {
+	// @ts-ignore
 	import(/* webpackChunkName: "main" */ __MAIN_ENTRY);
 });

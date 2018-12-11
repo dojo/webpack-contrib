ef1cb4789df386a3c90768cb8519ea0f5af072ec
Matt Gadd <mgadd@sitepen.com>
2018-12-11 12:46:58 +0000

    simplify loader

--- src/build-time-render/build-bridge-loader.ts
+++ src/build-time-render/build-bridge-loader.ts
@@ -6,20 +6,9 @@ module.exports = function(this: any, content: string) {
 	if (this.cacheable) {
 		this.cacheable();
 	}
-
 	const query = loaderUtils.getOptions(this) || {};
-	const imports: string[] = [];
-
-	Object.keys(query).forEach(function(name) {
-		let value;
-		if (typeof query[name] === 'string' && query[name].substr(0, 1) === '>') {
-			value = query[name].substr(1);
-		}
-		imports.push('var ' + name + ' = ' + value + ';');
-	});
-
-	const prefix = imports.join('\n') + '\n\n';
-	const modulePath = query.modulePath.substr(1);
+	const { modulePath } = query;
+	const prefix = `var modulePath = ${modulePath}\n\n`;
 	content = content.replace('{{ REPLACE }}', `build-bridge-cache ${modulePath}`);
 	return prefix + content;
 };

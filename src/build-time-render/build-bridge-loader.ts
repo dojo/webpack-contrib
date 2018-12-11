e9d7a5eba29912500deb45513bb260a3f2a3e481
Matt Gadd <mgadd@sitepen.com>
2018-12-11 12:16:34 +0000

    add loader

---
+++ src/build-time-render/build-bridge-loader.ts
@@ -0,0 +1,25 @@
+const loaderUtils = require('loader-utils');
+const SourceNode = require('source-map').SourceNode;
+const SourceMapConsumer = require('source-map').SourceMapConsumer;
+
+module.exports = function(content) {
+	if (this.cacheable) {
+		this.cacheable();
+	}
+
+	const query = loaderUtils.getOptions(this) || {};
+	const imports = [];
+
+	Object.keys(query).forEach(function(name) {
+		let value;
+		if (typeof query[name] === 'string' && query[name].substr(0, 1) === '>') {
+			value = query[name].substr(1);
+		}
+		imports.push('var ' + name + ' = ' + value + ';');
+	});
+
+	const prefix = imports.join('\n') + '\n\n';
+	const modulePath = query.modulePath.substr(1);
+	content = content.replace('{{ REPLACE }}', `build-bridge-cache ${modulePath}`);
+	return prefix + content;
+};

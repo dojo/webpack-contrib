3155897907207c711307d62669044ecd1c5f009d
Anthony Gubler <agubler@sitepen.com>
2019-06-04 13:41:38 +0100

    Use the public origin instead of the global origin

--- src/bootstrap-plugin/common.js
+++ src/bootstrap-plugin/common.js
@@ -12,7 +12,7 @@ if (!has.exists('build-serve')) {
 if (global.default.__public_path__ || global.default.__public_origin__) {
 	var publicPath = global.default.__public_origin__ || window.location.origin;
 	if (global.default.__public_path__) {
-		publicPath = origin + global.default.__public_path__;
+		publicPath = publicPath + global.default.__public_path__;
 		has.add('public-path', global.default.__public_path__, true);
 	}
 	__webpack_public_path__ = publicPath;

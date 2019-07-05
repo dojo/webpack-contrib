2203a79cbd437177d8f2af7e5ac3b4947ac0c6d5
Anthony Gubler <agubler@sitepen.com>
2019-07-05 10:23:22 +0100

    Fix static BTR when no paths are specified

---
+++ tests/support/fixtures/build-time-render/state-static-no-paths/main.js
@@ -0,0 +1,27 @@
+(function main() {
+	const app = document.getElementById('app');
+	const div = document.createElement('div');
+	const imgOne = document.createElement('img');
+	const imgTwo = document.createElement('img');
+	const imgThree = document.createElement('img');
+	const imgFour = document.createElement('img');
+	const imgFive = document.createElement('img');
+	div.classList.add('hello', 'another');
+	div.innerHTML = JSON.stringify(window.DojoHasEnvironment);
+	app.appendChild(div);
+
+	imgOne.setAttribute('src', 'https://example.com');
+	div.appendChild(imgOne);
+
+	imgTwo.setAttribute('src', 'http://example.com');
+	div.appendChild(imgTwo);
+
+	imgThree.setAttribute('src', '/image.svg');
+	div.appendChild(imgThree);
+
+	imgFour.setAttribute('src', './relative-image.svg');
+	div.appendChild(imgFour);
+
+	imgFive.setAttribute('src', '../other-relative-image.svg');
+	div.appendChild(imgFive);
+})();

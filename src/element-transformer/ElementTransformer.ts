f045f28c58e4aa9b9cd1b3c99dd66db93b7ae1c3
Bradley Maier <bmaier@sitepen.com>
2019-07-09 09:37:38 -0400

    Element transformer WIP

--- src/element-transformer/ElementTransformer.ts
+++ src/element-transformer/ElementTransformer.ts
@@ -37,75 +37,131 @@ export default function elementTransformer<T extends ts.Node>(
 				: [];
 			const classSymbol =
 				ts.isClassDeclaration(node) && node.name ? checker.getSymbolAtLocation(node.name) : undefined;
-			const classNode = node as ts.ClassDeclaration;
+			const variableSymbol =
+				ts.isVariableDeclaration(node) && node.name ? checker.getSymbolAtLocation(node.name) : undefined;
+
+			let widgetName: string = '';
+			const attributes: string[] = [];
+			const properties: string[] = [];
+			const events: string[] = [];
+			let propertyAccess: ts.PropertyAccessExpression | undefined = undefined;
+
+			function parsePropertyType(prop: ts.Symbol, locationNode: ts.Node) {
+				const type = checker.getTypeOfSymbolAtLocation(prop, locationNode);
+				const name = prop.getName();
+				let types = [type];
+
+				const intersectionType = type as ts.UnionOrIntersectionType;
+
+				if (intersectionType.types && intersectionType.types.length > 0) {
+					types = intersectionType.types;
+				}
+
+				types = types.filter((type) => checker.typeToString(type) !== 'undefined');
+
+				if (types.length === 1) {
+					if (
+						name.indexOf('on') === 0 &&
+						checker.getSignaturesOfType(types[0], ts.SignatureKind.Call).length > 0
+					) {
+						events.push(name);
+					} else if (checker.typeToString(types[0]) === 'string') {
+						attributes.push(name);
+					} else {
+						properties.push(name);
+					}
+				} else {
+					if (types.some((type) => Boolean(type.flags & ts.TypeFlags.StringLike))) {
+						attributes.push(name);
+					} else {
+						properties.push(name);
+					}
+				}
+			}
 
 			if (
 				customElementFilesIncludingDefaults.indexOf(path.resolve(node.getSourceFile().fileName)) !== -1 &&
 				defaultExport &&
+				variableSymbol &&
+				checker.getTypeOfSymbolAtLocation(defaultExport, node.getSourceFile()) ===
+					checker.getTypeOfSymbolAtLocation(variableSymbol, node)
+			) {
+				const variableNode = node as ts.VariableDeclaration;
+				const initializer = variableNode.initializer;
+				if (initializer && ts.isCallExpression(initializer)) {
+					const call = initializer as ts.CallExpression;
+					const renderOptionsCallback = call.arguments[0];
+					let optionsNode: ts.Node | undefined;
+					if (ts.isFunctionLike(renderOptionsCallback)) {
+						optionsNode = renderOptionsCallback.parameters[0];
+					} else if (ts.isIdentifier(renderOptionsCallback)) {
+						// TODO
+						// const type = checker.getTypeAtLocation(renderOptionsCallback);
+						// type.is
+						// ts.isFunctionTypeNode(type)
+						// (renderOptionsCallback as ts.Identifier).
+						optionsNode = undefined;
+					}
+
+					if (optionsNode) {
+						const typeOfOptions = checker.getTypeAtLocation(optionsNode);
+						const properties = typeOfOptions.getProperty('properties');
+						const typeOfProperties =
+							properties && checker.getTypeOfSymbolAtLocation(properties, optionsNode);
+						if (typeOfProperties) {
+							typeOfProperties.getProperties().forEach((prop) => {
+								parsePropertyType(prop, optionsNode!);
+							});
+							widgetName = variableNode.name!.getText();
+							propertyAccess = ts.createPropertyAccess(
+								ts.createIdentifier(widgetName),
+								'__customElementDescriptor'
+							);
+						}
+					}
+				}
+			} else if (
+				customElementFilesIncludingDefaults.indexOf(path.resolve(node.getSourceFile().fileName)) !== -1 &&
+				defaultExport &&
 				classSymbol &&
 				checker.getTypeOfSymbolAtLocation(defaultExport, node.getSourceFile()) ===
-					checker.getTypeOfSymbolAtLocation(classSymbol, node) &&
-				classNode.heritageClauses &&
-				classNode.heritageClauses.length > 0
+					checker.getTypeOfSymbolAtLocation(classSymbol, node)
 			) {
-				const widgetName = classNode.name!.getText();
+				const classNode = node as ts.ClassDeclaration;
+				if (classNode.heritageClauses && classNode.heritageClauses.length > 0) {
+					widgetName = classNode.name!.getText();
+
+					const [
+						{
+							types: [{ typeArguments = [] }]
+						}
+					] = classNode.heritageClauses;
+
+					if (typeArguments.length) {
+						const widgetPropNode = typeArguments[0];
+						const widgetPropNodeSymbol = checker.getSymbolAtLocation(widgetPropNode.getChildAt(0));
+
+						if (widgetPropNodeSymbol) {
+							const widgetPropType = checker.getDeclaredTypeOfSymbol(widgetPropNodeSymbol);
+							const widgetProps = widgetPropType.getProperties();
+
+							widgetProps.forEach((prop) => {
+								parsePropertyType(prop, widgetPropNode);
+							});
+						}
+					}
+
+					propertyAccess = ts.createPropertyAccess(
+						ts.createPropertyAccess(ts.createIdentifier(widgetName), ts.createIdentifier('prototype')),
+						'__customElementDescriptor'
+					);
+				}
+			}
+
+			if (widgetName && propertyAccess) {
 				const tagName = `${preparedElementPrefix}-${widgetName
 					.replace(/([a-z])([A-Z])/g, '$1-$2')
 					.toLowerCase()}`;
-
-				const [
-					{
-						types: [{ typeArguments = [] }]
-					}
-				] = classNode.heritageClauses;
-
-				const attributes: string[] = [];
-				const properties: string[] = [];
-				const events: string[] = [];
-
-				if (typeArguments.length) {
-					const widgetPropNode = typeArguments[0];
-					const widgetPropNodeSymbol = checker.getSymbolAtLocation(widgetPropNode.getChildAt(0));
-
-					if (widgetPropNodeSymbol) {
-						const widgetPropType = checker.getDeclaredTypeOfSymbol(widgetPropNodeSymbol);
-						const widgetProps = widgetPropType.getProperties();
-
-						widgetProps.forEach((prop) => {
-							const type = checker.getTypeOfSymbolAtLocation(prop, widgetPropNode);
-							const name = prop.getName();
-
-							let types = [type];
-
-							const intersectionType = type as ts.UnionOrIntersectionType;
-
-							if (intersectionType.types && intersectionType.types.length > 0) {
-								types = intersectionType.types;
-							}
-
-							types = types.filter((type) => checker.typeToString(type) !== 'undefined');
-
-							if (types.length === 1) {
-								if (
-									name.indexOf('on') === 0 &&
-									checker.getSignaturesOfType(types[0], ts.SignatureKind.Call).length > 0
-								) {
-									events.push(name);
-								} else if (checker.typeToString(types[0]) === 'string') {
-									attributes.push(name);
-								} else {
-									properties.push(name);
-								}
-							} else {
-								if (types.some((type) => Boolean(type.flags & ts.TypeFlags.StringLike))) {
-									attributes.push(name);
-								} else {
-									properties.push(name);
-								}
-							}
-						});
-					}
-				}
 
 				const customElementDeclaration = ts.createObjectLiteral([
 					ts.createPropertyAssignment('tagName', ts.createLiteral(tagName)),
@@ -123,13 +179,8 @@ export default function elementTransformer<T extends ts.Node>(
 					)
 				]);
 
-				const prototypeAccess = ts.createPropertyAccess(
-					ts.createPropertyAccess(ts.createIdentifier(widgetName), ts.createIdentifier('prototype')),
-					'__customElementDescriptor'
-				);
-
 				const existingDescriptor = ts.createBinary(
-					prototypeAccess,
+					propertyAccess,
 					ts.SyntaxKind.BarBarToken,
 					ts.createObjectLiteral()
 				);
@@ -138,7 +189,7 @@ export default function elementTransformer<T extends ts.Node>(
 					node,
 					ts.createStatement(
 						ts.createAssignment(
-							prototypeAccess,
+							propertyAccess,
 							ts.createObjectLiteral([
 								ts.createSpreadAssignment(customElementDeclaration),
 								ts.createSpreadAssignment(existingDescriptor)

299a61907c4613b7c3506590db00fa84f17cdbc8
Anthony Gubler <agubler@sitepen.com>
2019-09-27 16:20:33 +0100

    check scoped global before main global

--- src/bootstrap-plugin/common.js
+++ src/bootstrap-plugin/common.js
@@ -1,6 +1,10 @@
 var has = require('@dojo/framework/core/has');
 var global = require('@dojo/framework/shim/global');
 
+if (!global.default[__DOJO_SCOPE]) {
+	global.default[__DOJO_SCOPE] = {};
+}
+
 if (!has.exists('build-time-render')) {
 	has.add('build-time-render', false, false);
 }
@@ -9,13 +13,23 @@ if (!has.exists('build-serve')) {
 	has.add('build-serve', false, false);
 }
 
-has.add('app-base', global.default.__app_base__ || '/', true);
+var appBase = global.default[__DOJO_SCOPE].base ? global.default[__DOJO_SCOPE].base : global.default.__app_base__;
 
-if (global.default.__public_path__ || global.default.__public_origin__) {
-	var publicPath = global.default.__public_origin__ || window.location.origin;
-	if (global.default.__public_path__) {
-		publicPath = publicPath + global.default.__public_path__;
-		has.add('public-path', global.default.__public_path__, true);
+var initialPublicPath = global.default[__DOJO_SCOPE].publicPath
+	? global.default[__DOJO_SCOPE].publicPath
+	: global.default.__public_path__;
+
+var initialPublicOrigin = global.default[__DOJO_SCOPE].publicOrigin
+	? global.default[__DOJO_SCOPE].publicOrigin
+	: global.default.__public_origin__;
+
+has.add('app-base', appBase || '/', true);
+
+if (initialPublicPath || initialPublicOrigin) {
+	var publicPath = initialPublicOrigin || window.location.origin;
+	if (initialPublicPath) {
+		publicPath = publicPath + initialPublicPath;
+		has.add('public-path', initialPublicPath, true);
 	}
 	__webpack_public_path__ = publicPath;
 }

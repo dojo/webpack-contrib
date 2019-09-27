733b9e1da2fa7496fba5817c25811a2e6d496d06
Matthew Gadd <mgadd@sitepen.com>
2019-09-27 16:24:31 +0100

    use module exports over export default for amd convenience (#201)

--- src/bootstrap-plugin/async.js
+++ src/bootstrap-plugin/async.js
@@ -34,6 +34,6 @@ if (!has.default('dom-pointer-events')) {
 	modules.push(import(/* webpackChunkName: "runtime/pointerEvents" */ '@dojo/framework/shim/pointerEvents'));
 }
 
-export default Promise.all(modules).then(function() {
+module.exports = Promise.all(modules).then(function() {
 	return import(/* webpackChunkName: "main" */ __MAIN_ENTRY);
 });

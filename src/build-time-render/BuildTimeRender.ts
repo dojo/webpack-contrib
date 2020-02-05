492e214f83efd7fe8ac93ff31a4f17bb102dfc35
Anthony Gubler <agubler@sitepen.com>
2020-02-05 14:49:59 +0000

    Do not add blocks to static pages

--- src/build-time-render/BuildTimeRender.ts
+++ src/build-time-render/BuildTimeRender.ts
@@ -221,11 +221,12 @@ export default class BuildTimeRender {
 				);
 			}
 		}
-		blockScripts.forEach((blockScript, i) => {
-			writtenAssets.push(blockScript);
-
-			html = html.replace('</body>', `<script type="text/javascript" src="${blockScript}"></script></body>`);
-		});
+		if (!this._static && !staticPath) {
+			blockScripts.forEach((blockScript, i) => {
+				writtenAssets.push(blockScript);
+				html = html.replace('</body>', `<script type="text/javascript" src="${blockScript}"></script></body>`);
+			});
+		}
 		const htmlPath = join(this._output!, ...path.split('/'), 'index.html');
 		if (path) {
 			this._writtenHtmlFiles.push(htmlPath);

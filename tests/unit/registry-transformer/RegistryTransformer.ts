ce80317ab22c552d9baa9c5d876c2d04b4e58f1f
Anthony Gubler <agubler@sitepen.com>
2020-04-07 17:40:46 +0100

    Code split top level routes in defined in an Outlet (#259)
    * Change Outlet to Route
    
    * simple outlet detection
    
    * ensure is outlet children
    
    * add another widget with object children
    
    * add tsx support for outlet
    
    * Test other widgets with object children
    
    * Add test for nested children with matching route but not in an outlet
    
    * prettier
    
    * tidy
    
    * update framework version

--- tests/unit/registry-transformer/RegistryTransformer.ts
+++ tests/unit/registry-transformer/RegistryTransformer.ts
@@ -366,18 +366,18 @@ export default HelloWorld;
 				Quz: 'Quz'
 			},
 			modules: {
-				__autoRegistryItem_Bar: { path: 'widgets/Bar', outletName: [] },
-				__autoRegistryItem_Baz: { path: 'Baz', outletName: [] },
-				__autoRegistryItem_Quz: { path: 'Quz', outletName: [] }
+				__autoRegistryItem_Bar: { path: 'widgets/Bar', routeName: [] },
+				__autoRegistryItem_Baz: { path: 'Baz', routeName: [] },
+				__autoRegistryItem_Quz: { path: 'Quz', routeName: [] }
 			}
 		});
 	});
 
-	it('can distinguish widgets in an outlet renderer', () => {
+	it('can distinguish widgets in an route renderer', () => {
 		const source = `
 		import { v, w } from '@dojo/framework/core/vdom';
 		import WidgetBase from '@dojo/framework/core/WidgetBase';
-		import { Outlet } from '@dojo/framework/routing/Outlet';
+		import { Route } from '@dojo/framework/routing/Route';
 		import Bar from './widgets/Bar';
 		import Baz from './Baz';
 		import Quz from './Quz';
@@ -389,14 +389,14 @@ export default HelloWorld;
 				return v('div' [
 					v('div', ['Foo']),
 					w(Blah, {}),
-					w(Outlet, {
-						id: 'my-foo-outlet',
+					w(Route, {
+						id: 'my-foo-route',
 						renderer: () => {
 							return w(Something, {});
 						}
 					}),
-					w(Outlet, {
-						id: 'my-blah-outlet',
+					w(Route, {
+						id: 'my-blah-route',
 						renderer() {
 							return w(Blah, {});
 						}
@@ -419,8 +419,8 @@ export default HelloWorld;
 		export default HelloWorld;
 		`;
 		const transformer = registryTransformer(process.cwd(), ['widgets/Bar', 'Quz'], false, [
-			'my-foo-outlet',
-			'my-blah-outlet'
+			'my-foo-route',
+			'my-blah-route'
 		]);
 		const result = ts.transpileModule(source, {
 			compilerOptions: {
@@ -435,7 +435,7 @@ export default HelloWorld;
 
 		const expected = `import { v, w } from '@dojo/framework/core/vdom';
 import WidgetBase from '@dojo/framework/core/WidgetBase';
-import { Outlet } from '@dojo/framework/routing/Outlet';
+import { Route } from '@dojo/framework/routing/Route';
 import Baz from './Baz';
 import Blah from './Qux';
 var __autoRegistryItems = { Something: () => import("./Something"), Blah: () => import("./Qux"), Bar: () => import("./widgets/Bar"), Quz: () => import("./Quz") };
@@ -443,14 +443,14 @@ export class Foo extends WidgetBase {
     render() {
         return v('div'[v('div', ['Foo']),
             w(Blah, {}),
-            w(Outlet, {
-                id: 'my-foo-outlet',
+            w(Route, {
+                id: 'my-foo-route',
                 renderer: () => {
                     return w({ label: "__autoRegistryItem_Something", registryItem: __autoRegistryItems.Something }, {});
                 }
             }),
-            w(Outlet, {
-                id: 'my-blah-outlet',
+            w(Route, {
+                id: 'my-blah-route',
                 renderer() {
                     return w({ label: "__autoRegistryItem_Blah", registryItem: __autoRegistryItems.Blah }, {});
                 }
@@ -478,21 +478,128 @@ export default HelloWorld;
 				Something: 'Something'
 			},
 			modules: {
-				__autoRegistryItem_Bar: { path: 'widgets/Bar', outletName: [] },
-				__autoRegistryItem_Blah: { path: 'Qux', outletName: ['my-blah-outlet'] },
-				__autoRegistryItem_Quz: { path: 'Quz', outletName: [] },
-				__autoRegistryItem_Something: { outletName: ['my-foo-outlet'], path: 'Something' }
+				__autoRegistryItem_Bar: { path: 'widgets/Bar', routeName: [] },
+				__autoRegistryItem_Blah: { path: 'Qux', routeName: ['my-blah-route'] },
+				__autoRegistryItem_Quz: { path: 'Quz', routeName: [] },
+				__autoRegistryItem_Something: { routeName: ['my-foo-route'], path: 'Something' }
 			}
 		});
 	});
 
-	it('can distinguish widgets in an outlet renderer tsx', () => {
+	it('can distinguish widgets in outlet children', () => {
+		const source = `
+		import { v, w } from '@dojo/framework/core/vdom';
+		import WidgetBase from '@dojo/framework/core/WidgetBase';
+		import { Outlet } from '@dojo/framework/routing/Outlet';
+		import Bar from './widgets/Bar';
+		import Baz from './Baz';
+		import Quz from './Quz';
+        import Blah from './Qux';
+        import WithChildren from './WithChildren';
+		import Something from './Something';
+
+		export class Foo extends WidgetBase {
+			protected render() {
+				return v('div' [
+					v('div', ['Foo']),
+                    w(Blah, {}),
+                    w(Outlet, { id: 'main' }, [{
+                        'my-foo-route':  w(Something, {}),
+                        'my-blah-route': () => w(Blah, {})
+
+                    }]),
+                    w(WithChildren, {}, [{
+                        'my-foo-route': w(Baz, {})
+                    }]),
+					w(Bar, {}),
+					w(Baz, {})
+				]);
+			}
+		}
+
+		export class Another extends WidgetBase {
+			protected render() {
+				return v('div' [
+					w(Bar, {}),
+					w(Baz, {}),
+					w(Quz, {})
+				]);
+			}
+		}
+		export default HelloWorld;
+		`;
+		const transformer = registryTransformer(process.cwd(), ['widgets/Bar', 'Quz'], false, [
+			'my-foo-route',
+			'my-blah-route'
+		]);
+		const result = ts.transpileModule(source, {
+			compilerOptions: {
+				importHelpers: true,
+				module: ts.ModuleKind.ESNext,
+				target: ts.ScriptTarget.ESNext
+			},
+			transformers: {
+				before: [transformer]
+			}
+		});
+
+		const expected = `import { v, w } from '@dojo/framework/core/vdom';
+import WidgetBase from '@dojo/framework/core/WidgetBase';
+import { Outlet } from '@dojo/framework/routing/Outlet';
+import Baz from './Baz';
+import Blah from './Qux';
+import WithChildren from './WithChildren';
+var __autoRegistryItems = { Something: () => import("./Something"), Blah: () => import("./Qux"), Bar: () => import("./widgets/Bar"), Quz: () => import("./Quz") };
+export class Foo extends WidgetBase {
+    render() {
+        return v('div'[v('div', ['Foo']),
+            w(Blah, {}),
+            w(Outlet, { id: 'main' }, [{
+                    'my-foo-route': w({ label: "__autoRegistryItem_Something", registryItem: __autoRegistryItems.Something }, {}),
+                    'my-blah-route': () => w({ label: "__autoRegistryItem_Blah", registryItem: __autoRegistryItems.Blah }, {})
+                }]),
+            w(WithChildren, {}, [{
+                    'my-foo-route': w(Baz, {})
+                }]),
+            w({ label: "__autoRegistryItem_Bar", registryItem: __autoRegistryItems.Bar }, {}),
+            w(Baz, {})]);
+    }
+}
+export class Another extends WidgetBase {
+    render() {
+        return v('div'[w({ label: "__autoRegistryItem_Bar", registryItem: __autoRegistryItems.Bar }, {}),
+            w(Baz, {}),
+            w({ label: "__autoRegistryItem_Quz", registryItem: __autoRegistryItems.Quz }, {})]);
+    }
+}
+export default HelloWorld;
+`;
+		assert.equal(nl(result.outputText), expected);
+		assert.deepEqual(shared, {
+			all: {
+				Bar: 'widgets/Bar',
+				Baz: 'Baz',
+				Blah: 'Qux',
+				Quz: 'Quz',
+				Something: 'Something',
+				WithChildren: 'WithChildren'
+			},
+			modules: {
+				__autoRegistryItem_Bar: { path: 'widgets/Bar', routeName: [] },
+				__autoRegistryItem_Blah: { path: 'Qux', routeName: ['my-blah-route'] },
+				__autoRegistryItem_Quz: { path: 'Quz', routeName: [] },
+				__autoRegistryItem_Something: { routeName: ['my-foo-route'], path: 'Something' }
+			}
+		});
+	});
+
+	it('can distinguish widgets in an route renderer tsx', () => {
 		const source = `
 import WidgetBase from '@dojo/framework/core/WidgetBase';
 import Bar from './widgets/Bar';
 import Baz from './Baz';
 import Blah from './Qux';
-import Outlet from '@dojo/framework/routing/Outlet';
+import Route from '@dojo/framework/routing/Route';
 
 export class Foo extends WidgetBase {
 	protected render() {
@@ -504,15 +611,15 @@ export class Foo extends WidgetBase {
 					<Baz>
 						<div>child</div>
 					</Baz>
-					<Outlet id="my-bar-outlet" renderer={ () => (<Bar />) } />
-					<Outlet id="my-blah-outlet" renderer={ () => (<Blah />) } />
+					<Route id="my-bar-route" renderer={ () => (<Bar />) } />
+					<Route id="my-blah-route" renderer={ () => (<Blah />) } />
 				</div>
 			</div>
 		);
 	}
 }
 `;
-		const transformer = registryTransformer(process.cwd(), [], false, ['my-bar-outlet', 'my-blah-outlet']);
+		const transformer = registryTransformer(process.cwd(), [], false, ['my-bar-route', 'my-blah-route']);
 		const result = ts.transpileModule(source, {
 			compilerOptions: {
 				importHelpers: true,
@@ -529,7 +636,7 @@ export class Foo extends WidgetBase {
 		const expected = `import WidgetBase from '@dojo/framework/core/WidgetBase';
 import Baz from './Baz';
 import Blah from './Qux';
-import Outlet from '@dojo/framework/routing/Outlet';
+import Route from '@dojo/framework/routing/Route';
 var Loadable__ = { type: "registry" };
 var __autoRegistryItems = { Bar: () => import("./widgets/Bar"), Blah: () => import("./Qux") };
 export class Foo extends WidgetBase {
@@ -541,8 +648,8 @@ export class Foo extends WidgetBase {
 					<Baz>
 						<div>child</div>
 					</Baz>
-					<Outlet id="my-bar-outlet" renderer={() => (<Loadable__ __autoRegistryItem={{ label: "__autoRegistryItem_Bar", registryItem: __autoRegistryItems.Bar }}/>)}/>
-					<Outlet id="my-blah-outlet" renderer={() => (<Loadable__ __autoRegistryItem={{ label: "__autoRegistryItem_Blah", registryItem: __autoRegistryItems.Blah }}/>)}/>
+					<Route id="my-bar-route" renderer={() => (<Loadable__ __autoRegistryItem={{ label: "__autoRegistryItem_Bar", registryItem: __autoRegistryItems.Bar }}/>)}/>
+					<Route id="my-blah-route" renderer={() => (<Loadable__ __autoRegistryItem={{ label: "__autoRegistryItem_Blah", registryItem: __autoRegistryItems.Blah }}/>)}/>
 				</div>
 			</div>);
     }
@@ -556,8 +663,103 @@ export class Foo extends WidgetBase {
 				Blah: 'Qux'
 			},
 			modules: {
-				__autoRegistryItem_Bar: { path: 'widgets/Bar', outletName: ['my-bar-outlet'] },
-				__autoRegistryItem_Blah: { path: 'Qux', outletName: ['my-blah-outlet'] }
+				__autoRegistryItem_Bar: { path: 'widgets/Bar', routeName: ['my-bar-route'] },
+				__autoRegistryItem_Blah: { path: 'Qux', routeName: ['my-blah-route'] }
+			}
+		});
+	});
+
+	it('can distinguish widgets in outlet children tsx', () => {
+		const source = `
+import WidgetBase from '@dojo/framework/core/WidgetBase';
+import Bar from './widgets/Bar';
+import Baz from './Baz';
+import Blah from './Qux';
+import Something from './Something';
+import Outlet from '@dojo/framework/routing/Outlet';
+
+export class Foo extends WidgetBase {
+	protected render() {
+		return (
+			<div>
+				<div>
+					<Blah />
+					<div>Foo</div>
+					<Baz>
+						<div>child</div>
+                    </Baz>
+                    <Something>{{
+                        'my-bar-route': <Baz />
+                    }}</Something>
+                    <Outlet id="main">{{
+                        'other': <Something>{{ 'my-bar-route': <Baz /> }}</Something>
+                    }}</Outlet>
+                    <Outlet id="main">{{
+                        'my-bar-route': <Bar />,
+                        'my-blah-route': () => <Blah />
+                    }}</Outlet>
+				</div>
+			</div>
+		);
+	}
+}
+`;
+		const transformer = registryTransformer(process.cwd(), [], false, ['my-bar-route', 'my-blah-route']);
+		const result = ts.transpileModule(source, {
+			compilerOptions: {
+				importHelpers: true,
+				module: ts.ModuleKind.ESNext,
+				target: ts.ScriptTarget.ESNext,
+				jsx: ts.JsxEmit.Preserve,
+				jsxFactory: 'tsx'
+			},
+			transformers: {
+				before: [transformer]
+			}
+		});
+
+		const expected = `import WidgetBase from '@dojo/framework/core/WidgetBase';
+import Baz from './Baz';
+import Blah from './Qux';
+import Something from './Something';
+import Outlet from '@dojo/framework/routing/Outlet';
+var Loadable__ = { type: "registry" };
+var __autoRegistryItems = { Bar: () => import("./widgets/Bar"), Blah: () => import("./Qux") };
+export class Foo extends WidgetBase {
+    render() {
+        return (<div>
+				<div>
+					<Blah />
+					<div>Foo</div>
+					<Baz>
+						<div>child</div>
+                    </Baz>
+                    <Something>{{
+            'my-bar-route': <Baz />
+        }}</Something>
+                    <Outlet id="main">{{
+            'other': <Something>{{ 'my-bar-route': <Baz /> }}</Something>
+        }}</Outlet>
+                    <Outlet id="main">{{
+            'my-bar-route': <Loadable__ __autoRegistryItem={{ label: "__autoRegistryItem_Bar", registryItem: __autoRegistryItems.Bar }}/>,
+            'my-blah-route': () => <Loadable__ __autoRegistryItem={{ label: "__autoRegistryItem_Blah", registryItem: __autoRegistryItems.Blah }}/>
+        }}</Outlet>
+				</div>
+			</div>);
+    }
+}
+`;
+		assert.equal(nl(result.outputText), expected);
+		assert.deepEqual(shared, {
+			all: {
+				Bar: 'widgets/Bar',
+				Baz: 'Baz',
+				Blah: 'Qux',
+				Something: 'Something'
+			},
+			modules: {
+				__autoRegistryItem_Bar: { path: 'widgets/Bar', routeName: ['my-bar-route'] },
+				__autoRegistryItem_Blah: { path: 'Qux', routeName: ['my-blah-route'] }
 			}
 		});
 	});
@@ -601,9 +803,9 @@ export default HelloWorld;
 `;
 		assert.equal(nl(result.outputText), expected);
 		assert.deepEqual(shared.modules, {
-			__autoRegistryItem_Bar: { path: 'widgets/Bar', outletName: [] },
-			__autoRegistryItem_Baz: { path: 'Baz', outletName: [] },
-			__autoRegistryItem_Quz: { path: 'Quz', outletName: [] }
+			__autoRegistryItem_Bar: { path: 'widgets/Bar', routeName: [] },
+			__autoRegistryItem_Baz: { path: 'Baz', routeName: [] },
+			__autoRegistryItem_Quz: { path: 'Quz', routeName: [] }
 		});
 	});
 });

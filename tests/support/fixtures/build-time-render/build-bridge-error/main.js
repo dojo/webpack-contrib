a64b7a9119a66b0a120c5c402a7602f8f6bd618c
Anthony Gubler <agubler@sitepen.com>
2020-05-22 09:04:18 +0100

    use worker for running btr blocks (#269)

--- tests/support/fixtures/build-time-render/build-bridge-error/main.js
+++ tests/support/fixtures/build-time-render/build-bridge-error/main.js
@@ -1,15 +1,16 @@
 "use strict";
 (function main() {
+    window.test = {}
     var app = document.getElementById('app');
     var div = document.createElement('div');
     /** @preserve dojoBuildBridgeCache '0' **/
     window.__dojoBuildBridge('foo.block', '0', ['a']).then(function (result) {
         div.innerHTML = result;
+        window.test.blocksPending = 0;
     });
+    window.test.blocksPending = 1;
     app.appendChild(div);
-	window.test = {
-		rendering: false
-	}
+	window.test.rendering = false;
 	throw new Error('runtime error');
 })();
 //# sourceMappingURL=main.js.map
--- tests/support/fixtures/build-time-render/build-bridge-single-bundle/expected/main.js
+++ tests/support/fixtures/build-time-render/build-bridge-single-bundle/expected/main.js
@@ -8,15 +8,16 @@ window.blockCacheEntry = function (modulePath, args, content) {
 };
 /** @preserve APPEND_BLOCK_CACHE_ENTRY **/ blockCacheEntry('foo.block', '["a"]', "hello world a");
 (function main() {
+    window.test = {}
     var app = document.getElementById('app');
     var div = document.createElement('div');
     /** @preserve dojoBuildBridgeCache 'foo.block' **/
     window.__dojoBuildBridge('foo.block', ['a']).then(function (result) {
         div.innerHTML = result;
+        window.test.blocksPending = 0;
     });
+    window.test.blocksPending = 1;
     app.appendChild(div);
-	window.test = {
-		rendering: false
-	}
+	window.test.rendering = false;
 })();
 //# sourceMappingURL=main.js.map

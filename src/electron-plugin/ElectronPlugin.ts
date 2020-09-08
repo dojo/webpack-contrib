bad3795b972d14b311cfd98ad269007058e0785e
rorticus <rmulligan@sitepen.com>
2020-09-08 12:29:36 -0400

    Electron plugin packaging (#303)
    * Revert "Remove Electron packaging (#247)"
    
    This reverts commit 3ac6ac0b961ebe52edb8a281bd04190131f09f56.

--- src/electron-plugin/ElectronPlugin.ts
+++ src/electron-plugin/ElectronPlugin.ts
@@ -1,22 +1,40 @@
+import * as path from 'path';
 import * as webpack from 'webpack';
+import * as fs from 'fs';
+
+const packager = require('electron-packager');
 
 interface ElectronPluginOptions {
 	electron: {
 		browser: any;
+		packaging: any;
 	};
+	dist: boolean;
 	watch: boolean;
 	serve: boolean;
 	port: number;
+	basePath: string;
+	outputPath: string;
 }
 
 export class ElectronPlugin {
+	private _packageJson: any = {};
 	private _options: ElectronPluginOptions;
 	private _defaultOptions: ElectronPluginOptions = {
+		dist: false,
 		watch: false,
 		serve: false,
 		port: 9999,
+		basePath: process.cwd(),
+		outputPath: 'output/dist',
 		electron: {
-			browser: { width: 800, height: 600 }
+			browser: { width: 800, height: 600 },
+			packaging: {
+				dir: 'output/dist',
+				app: 'app',
+				out: 'output/app',
+				overwrite: true
+			}
 		}
 	};
 
@@ -26,13 +44,22 @@ export class ElectronPlugin {
 			...this._defaultOptions.electron.browser,
 			...this._options.electron.browser
 		};
+		this._options.electron.packaging = {
+			...this._defaultOptions.electron.packaging,
+			...this._options.electron.packaging
+		};
+		const { basePath } = this._options;
+		const packageJsonPath = path.join(basePath, 'package.json');
+		this._packageJson = fs.existsSync(packageJsonPath) ? require(packageJsonPath) : {};
 	}
 	apply(compiler: webpack.Compiler) {
 		const {
-			electron: { browser },
+			electron: { browser, packaging },
+			dist,
 			watch,
 			serve,
-			port
+			port,
+			outputPath
 		} = this._options;
 
 		const definePlugin = new webpack.DefinePlugin({
@@ -42,6 +69,16 @@ export class ElectronPlugin {
 		});
 
 		definePlugin.apply(compiler);
+
+		compiler.hooks.done.tapAsync(this.constructor.name, (stats: any, callback: any) => {
+			const newPackageJson = { ...this._packageJson, main: 'main.electron.js' };
+			fs.writeFileSync(path.resolve(outputPath, 'package.json'), JSON.stringify(newPackageJson));
+			if (dist && !watch) {
+				packager({ quiet: true, ...packaging, app: this._packageJson.name }).then(() => callback());
+			} else {
+				callback();
+			}
+		});
 	}
 }
 
